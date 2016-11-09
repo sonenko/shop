312c61968a27e89e1bc932f0e313349f9b12f4c6
sonenko <growler.ua@gmail.com>
2016-11-09 23:42:19 +0200

    fix #42 fix response codes

--- src/main/scala/com/github/sonenko/shoppingbasket/rest/ShoppingBasketRoute.scala
+++ src/main/scala/com/github/sonenko/shoppingbasket/rest/ShoppingBasketRoute.scala
@@ -9,50 +9,51 @@ import com.github.sonenko.shoppingbasket.basketmanager.BasketManagerActor
 import com.github.sonenko.shoppingbasket.basketmanager.basket.BasketActor
 
 
-trait ShoppingBasketRoute {
-  this: RootRoute =>
+trait ShoppingBasketRoute { this: RootRoute =>
 
-  def cook = HttpCookie(Config.cookieNameForSession, java.util.UUID.randomUUID().toString)
+  def newCookie = HttpCookie(Config.cookieNameForSession, java.util.UUID.randomUUID().toString)
 
   def shoppingBasketRoute: Route = pathPrefix("api" / "shoppingbasket") {
-    cookie(Config.cookieNameForSession) { cook =>
-      val basketId = java.util.UUID.fromString(cook.value)
-      pathEndOrSingleSlash {
-        get {
-          complete {
-            inquire(basketManager.actor, BasketManagerActor.Commands.ToBasket(
-              basketId,
-              BasketActor.Commands.GetState,
-              false
-            ))
-          }
-        } ~
-        post {
-          entity(as[AddGood]) { addGood =>
+    optionalCookie(Config.cookieNameForSession) {
+      case None =>
+        setCookie(newCookie) { ctx =>
+          redirect(ctx.request.uri, StatusCodes.PermanentRedirect).apply(ctx)
+        }
+      case Some(cook) =>
+        val basketId = java.util.UUID.fromString(cook.value)
+        pathEndOrSingleSlash {
+          get {
             complete {
               inquire(basketManager.actor, BasketManagerActor.Commands.ToBasket(
                 basketId,
-                BasketActor.Commands.AddGood(addGood.goodId, addGood.count),
-                true
-              ))
-            }
-          }
-        } ~
-        delete {
-          entity(as[DropGood]) { dropGood =>
-            complete {
-              inquire(basketManager.actor, BasketManagerActor.Commands.ToBasket(
-                basketId,
-                BasketActor.Commands.DropGood(dropGood.goodId, dropGood.count),
+                BasketActor.Commands.GetState,
                 false
               ))
             }
+          } ~
+          post {
+            entity(as[AddGood]) { addGood =>
+              complete {
+                inquire(basketManager.actor, BasketManagerActor.Commands.ToBasket(
+                  basketId,
+                  BasketActor.Commands.AddGood(addGood.goodId, addGood.count),
+                  true
+                ))
+              }
+            }
+          } ~
+          delete {
+            entity(as[DropGood]) { dropGood =>
+              complete {
+                inquire(basketManager.actor, BasketManagerActor.Commands.ToBasket(
+                  basketId,
+                  BasketActor.Commands.DropGood(dropGood.goodId, dropGood.count),
+                  false
+                ))
+              }
+            }
           }
         }
-      }
-    } ~
-    setCookie(cook) { ctx =>
-      redirect(ctx.request.uri, StatusCodes.PermanentRedirect).apply(ctx)
     }
   }
 }
